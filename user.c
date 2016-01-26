256e7a25484f4b6f8f30b9d65314b7b73908b2fd
Keerthan Jaic <jckeerthan@gmail.com>
2016-01-26 14:23:49 -0500

    lab 2 user code

--- user.c
+++ user.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
+#include <errno.h>
+#include <math.h>
+#include <sys/mman.h>
 #include <fcntl.h>
+#include <unistd.h>
+
+struct u_kyouko_device {
+  unsigned int *u_control_base;
+} kyouko3;
+
+#define KYOUKO_CONTROL_SIZE (65536)
+#define Device_RAM (0x0020)
+
+unsigned int U_READ_REG(unsigned int reg) {
+  return (*(kyouko3.u_control_base+(reg>>2)));
+}
 
 int main() {
-  printf("user code: open\n");
+  int result;
   int fd = open("/dev/kyouko3", O_RDWR);
-  printf("user code: close\n");
+  kyouko3.u_control_base = mmap(0, KYOUKO_CONTROL_SIZE, PROT_READ|PROT_WRITE,
+      MAP_SHARED, fd, 0);
+  result = U_READ_REG(Device_RAM);
+
+  printf("Ram size is %n MB\n", result);
   close(fd);
+  return 0;
 }
