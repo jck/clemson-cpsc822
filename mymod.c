aa1cadde9ad10ab5e9cfef58a270a777dfce8863
Keerthan Jaic <jckeerthan@gmail.com>
2016-01-28 03:06:16 -0500

    Revert "use readl and writel for registers"
    This reverts commit 1d0ffca163836f528c3608fbc4d9cf847033e05c.

--- mymod.c
+++ mymod.c
@@ -29,11 +29,11 @@ struct kyouko3_vars {
 
 
 inline void K_WRITE_REG(unsigned int reg, unsigned int value) {
-	writel(value, kyouko3.control.k_base+(reg>>2));
+	*(kyouko3.control.k_base+(reg>>2)) = value;
 }
 
 inline unsigned int K_READ_REG(unsigned int reg){
-	return readl(kyouko3.control.k_base+(reg>>2));
+	return *(kyouko3.control.k_base+(reg>>2));
 }
 
 int kyouko3_open(struct inode *inode, struct file *fp) {
