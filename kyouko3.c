979ddfd514cfa8b806d68e9c07d783b073d6d75c
Tyler Allen <tnallen@g.clemson.edu>
2016-02-28 13:54:55 -0500

    Reverting last commit

--- kyouko3.c
+++ kyouko3.c
@@ -114,15 +114,15 @@ irqreturn_t dma_isr(int irq, void *dev_id, struct pt_regs *regs){
   printk(KERN_ALERT "k3.fill = %d, k3.drain = %d", k3.fill, k3.drain);
   full = k3.fill == k3.drain;
   k3.drain = (k3.drain + 1) % DMA_BUFNUM;
-  //empty = k3.fill == k3.drain;
-  //if (!empty)
-  //{
+  empty = k3.fill == k3.drain;
+  if (!empty)
+  {
     printk(KERN_ALERT "DMA Queue not empty in handler"); 
     size = ((struct kyouko3_dma_hdr*)(dma[k3.drain].k_base))->count;
     fifo_write(BUFA_ADDR, dma[k3.drain].handle);
     fifo_write(BUFA_CONF, size);
     K_WRITE_REG(FIFO_HEAD, k3.fifo.head);
-  //}
+  }
   if (full)
   {
     printk(KERN_ALERT "DMA Queue full in handler"); 
