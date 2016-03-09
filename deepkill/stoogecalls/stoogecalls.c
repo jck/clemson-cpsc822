cce438a353c15b9561fecf74cf46c5cd80b95f3a
Keerthan Jaic <jckeerthan@gmail.com>
2016-03-08 20:31:20 -0500

    add deepsleep syscall

---
+++ deepkill/stoogecalls/stoogecalls.c
@@ -0,0 +1,29 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/printk.h>
+
+__sched int sleep_on(wait_queue_head_t *q)
+{
+    unsigned long flags;
+    wait_queue_t wait;
+
+    init_waitqueue_entry(&wait, current);
+    __set_current_state(TASK_UNINTERRUPTIBLE);
+    spin_lock_irqsave(&q->lock, flags);
+    __add_wait_queue(q, &wait);
+    spin_unlock(&q->lock);
+    schedule();
+    spin_lock_irq(&q->lock);
+    __remove_wait_queue(q, &wait);
+    spin_unlock_irqrestore(&q->lock, flags);
+    return 0;
+}
+
+DECLARE_WAIT_QUEUE_HEAD(gone);
+
+
+SYSCALL_DEFINE0(deepsleep)
+{
+  return sleep_on(&gone);
+}
