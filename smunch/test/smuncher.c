b72038f25fea63aabb5db951e0f5954809531a2a
Keerthan Jaic <jckeerthan@gmail.com>
2016-04-04 11:38:57 -0400

    user: fix smuch def

--- smunch/test/smuncher.c
+++ smunch/test/smuncher.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 #define deepsleep() syscall(325)
-#define smunch(pid, sig) syscall(326, pid, bit_pattern)
+#define smunch(pid, bit_pattern) syscall(326, pid, bit_pattern)
 
 int main()
 {
