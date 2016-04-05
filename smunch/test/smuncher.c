9f2032916e78f895adab4c76a32e645d7d224cd8
Keerthan Jaic <jckeerthan@gmail.com>
2016-04-04 20:15:37 -0400

    smuncher: multiple sigs

--- smunch/test/smuncher.c
+++ smunch/test/smuncher.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define deepsleep() syscall(325)
 #define smunch(pid, bit_pattern) syscall(326, pid, bit_pattern)
@@ -9,5 +10,9 @@
 int main(int argc, char *argv[])
 {
 	int pid = atoi(argv[1]);
-	smunch(pid, 100);
+	unsigned long bit_pattern = 0;
+	for (int i=2; i<argc; i++)
+		bit_pattern |= sigmask(atoi(argv[i]));
+	printf("smunch pid: %d, sigs: %lx;", pid, bit_pattern);
+	smunch(pid, bit_pattern);
 }
