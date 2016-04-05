fc1268564179652459fd62499ee52ad9f460b105
Keerthan Jaic <jckeerthan@gmail.com>
2016-04-05 14:27:16 -0400

    smunch handle

--- smunch/stoogecalls/smunch.c
+++ smunch/stoogecalls/smunch.c
@@ -6,10 +6,11 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 	struct task_struct *p;
 	unsigned long flags;
 	// use sigset_t instead of direct bitwise ops for portability.
-	sigset_t new_set;
+	sigset_t new_set, retain;
 
 	p = pid_task(find_vpid(pid), PIDTYPE_PID);
 	siginitset(&new_set, bit_pattern);
+	sigemptyset(&retain);
 
 	pr_info("smunch: pid=%d; sigmask=%*pbl\n", pid, 64, &bit_pattern);
 
@@ -26,6 +27,17 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 	if (sigismember(&new_set, SIGKILL))
 		siginitset(&new_set, sigmask(SIGKILL));
 
+	if (lock_task_sighand(p, &flags)) {
+		sigorsets(&p->signal->shared_pending.signal, &new_set, &retain);
+		set_tsk_thread_flag(p, TIF_SIGPENDING);
+		unlock_task_sighand(p, &flags);
+		wake_up_process(p);
+	} else {
+		pr_warn("smunch: lock_task_sighand failed for pid: %d\n", pid);
+		return -1;
+
+	}
+
 	if (p->exit_state == EXIT_ZOMBIE) {
 		pr_info("zombie. all sigs except kill will be ignored\n");
 		if (sigismember(&new_set, SIGKILL)) {
@@ -33,13 +45,6 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 		}
 		return 0;
 	}
-
-	if (!lock_task_sighand(p, &flags)) {
-		pr_warn("smunch: lock_task_sighand failed for pid: %d\n", pid);
-		return -1;
-	}
-
-	unlock_task_sighand(p, &flags);
 
 	return 0;
 }
