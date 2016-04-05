fcc2859b2592cd67872902babdd5d6b44dc510b4
Keerthan Jaic <jckeerthan@gmail.com>
2016-04-05 14:29:20 -0400

    deepsleeper

---
+++ smunch/test/deepsleeper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#define deepsleep() syscall(325)  // ...or whatever number is next
+
+int main()
+{
+    printf("goodnight, Irene\n");
+    deepsleep();
+    printf("oops ... woke up!\n");
+}
